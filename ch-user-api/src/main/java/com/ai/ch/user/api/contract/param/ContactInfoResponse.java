d1d43bec8e898741f632856bc77680f3afbdf4e0
zhangyh7 <zhangyh7@zhangyh7.ai.com>
2016-08-17 15:38:56 +0800

    合同入参和出参对象

---
+++ ch-user-api/src/main/java/com/ai/ch/user/api/contract/param/ContactInfoResponse.java
@@ -0,0 +1,85 @@
+package com.ai.ch.user.api.contract.param;
+
+import java.sql.Timestamp;
+
+import com.ai.opt.base.vo.BaseResponse;
+
+public class ContactInfoResponse  extends BaseResponse{
+
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 用户Id
+	 */
+	private String userId;
+	/**
+	 * 合同编号
+	 */
+	private String contractCode;
+	/**
+	 * 合同名称
+	 */
+	private String contractName;
+	/**
+	 * 合同类型
+	 * 1、供应商合同
+	 * 2、店铺合同';
+	 */
+	private String contractType;
+	/**
+	 * 开始时间
+	 */
+	private Timestamp activeTime;
+	/**
+	 * 结束时间
+	 */
+	private Timestamp inactiveTime;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getContractCode() {
+		return contractCode;
+	}
+
+	public void setContractCode(String contractCode) {
+		this.contractCode = contractCode;
+	}
+
+	public String getContractName() {
+		return contractName;
+	}
+
+	public void setContractName(String contractName) {
+		this.contractName = contractName;
+	}
+
+	public String getContractType() {
+		return contractType;
+	}
+
+	public void setContractType(String contractType) {
+		this.contractType = contractType;
+	}
+
+	public Timestamp getActiveTime() {
+		return activeTime;
+	}
+
+	public void setActiveTime(Timestamp activeTime) {
+		this.activeTime = activeTime;
+	}
+
+	public Timestamp getInactiveTime() {
+		return inactiveTime;
+	}
+
+	public void setInactiveTime(Timestamp inactiveTime) {
+		this.inactiveTime = inactiveTime;
+	}
+	
+}
