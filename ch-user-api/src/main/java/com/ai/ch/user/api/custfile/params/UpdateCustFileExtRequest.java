50b5e5ed09bdc03917b929b85f30d58d62120b29
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-08-24 20:28:59 +0800

    添加userId和infoItem属性

--- ch-user-api/src/main/java/com/ai/ch/user/api/custfile/params/UpdateCustFileExtRequest.java
+++ ch-user-api/src/main/java/com/ai/ch/user/api/custfile/params/UpdateCustFileExtRequest.java
@@ -8,8 +8,22 @@ import com.ai.opt.base.vo.BaseInfo;
 public class UpdateCustFileExtRequest extends BaseInfo{
 
     private static final long serialVersionUID = 1L;
+    /**
+     * 用户Id
+     */
+    private String userId;
+    
+    /**
+     * 条目
+     * 30001 供应商扫描合同
+     * 30002 供应商电子合同
+     * 40001 店铺扫描合同
+     * 40002 店铺电子合同
+     */
+    private String infoItem;
+    
     List<CmCustFileExtVo> list = new ArrayList<CmCustFileExtVo>();
-
+    
     public List<CmCustFileExtVo> getList() {
         return list;
     }
@@ -17,5 +31,21 @@ public class UpdateCustFileExtRequest extends BaseInfo{
     public void setList(List<CmCustFileExtVo> list) {
         this.list = list;
     }
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getInfoItem() {
+		return infoItem;
+	}
+
+	public void setInfoItem(String infoItem) {
+		this.infoItem = infoItem;
+	}
 
 }
