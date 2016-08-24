932211e2c6f4128086d696a5f19fad4ac5ff36bc
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-08-24 15:06:31 +0800

    新增insertCustFileExt

--- ch-user-core/src/main/java/com/ai/ch/user/service/business/interfaces/IContractBusiSV.java
+++ ch-user-core/src/main/java/com/ai/ch/user/service/business/interfaces/IContractBusiSV.java
@@ -3,10 +3,13 @@ package com.ai.ch.user.service.business.interfaces;
 
 import com.ai.ch.user.api.contract.param.ContactInfoRequest;
 import com.ai.ch.user.api.contract.param.ContactInfoResponse;
+import com.ai.ch.user.api.contract.param.InsertCustFileExtRequest;
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 
 public interface IContractBusiSV {
 	public int insertContractInfo(ContactInfoRequest  ctContractInfo)throws BusinessException, SystemException;
 	public ContactInfoResponse queryContractInfo(ContactInfoRequest contactInfoRequest)throws BusinessException, SystemException;
+	public void insertCustFileExt(InsertCustFileExtRequest request)throws SystemException, BusinessException;
+
 }
