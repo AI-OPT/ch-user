7b02fdc1bb1927eba8d33e41bf454adc2bda55e3
zh <zhangqiang7@asiainfo.com>
2016-08-25 15:17:36 +0800

    更新附件方法

--- ch-user-core/src/test/java/com/ai/ch/user/custfile/impl/test/CustFileSVImpl.java
+++ ch-user-core/src/test/java/com/ai/ch/user/custfile/impl/test/CustFileSVImpl.java
@@ -1,5 +1,8 @@
 package com.ai.ch.user.custfile.impl.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +10,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ai.ch.user.api.custfile.interfaces.ICustFileSV;
+import com.ai.ch.user.api.custfile.params.CmCustFileExtVo;
 import com.ai.ch.user.api.custfile.params.QueryCustFileExtRequest;
+import com.ai.ch.user.api.custfile.params.UpdateCustFileExtRequest;
 import com.alibaba.fastjson.JSON;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -22,5 +27,18 @@ public class CustFileSVImpl {
 		QueryCustFileExtRequest re = new QueryCustFileExtRequest();
 		re.setTenantId("ch");
 		System.out.println(JSON.toJSONString(custFileSV.queryCustFileExt(re)));
+	}
+	@Test
+	public void testUpdate(){
+		UpdateCustFileExtRequest re = new UpdateCustFileExtRequest();
+		re.setTenantId("ch");
+		List<CmCustFileExtVo> list = new ArrayList<CmCustFileExtVo>();
+		CmCustFileExtVo c = new CmCustFileExtVo();
+		c.setTenantId("ch");
+		list.add(c);
+		list.add(c);
+		list.add(c);
+		re.setList(list);
+		System.out.println(JSON.toJSONString(custFileSV.updateCustFileExt(re)));
 	}
 }
