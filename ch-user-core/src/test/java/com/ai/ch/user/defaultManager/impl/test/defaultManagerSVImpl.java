3b1b94db2523cc77fb3ea419c5c46203b5151337
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-08-25 17:17:04 +0800

    新增扣款测试类

---
+++ ch-user-core/src/test/java/com/ai/ch/user/defaultManager/impl/test/defaultManagerSVImpl.java
@@ -0,0 +1,35 @@
+package com.ai.ch.user.defaultManager.impl.test;
+
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.ai.ch.user.api.defaultlog.interfaces.IDefaultLogSV;
+import com.ai.ch.user.api.defaultlog.params.InsertDefaultLogRequest;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:/context/core-context.xml" })
+public class defaultManagerSVImpl {
+
+	@Autowired
+	private IDefaultLogSV defaultLogSV;
+	
+	@Test
+	public void insertDetaultInfoTest(){
+		InsertDefaultLogRequest request = new InsertDefaultLogRequest();
+		request.setUserId("1");
+		request.setDefaultReason("aaa");
+		request.setDeductBalance(1212L);
+		request.setDeductDate(new Timestamp(new Date().getTime()));
+		request.setOperId(1L);
+		request.setSerialCode("1111111111");
+		defaultLogSV.insertDefaultLog(request);
+	}
+	
+}
