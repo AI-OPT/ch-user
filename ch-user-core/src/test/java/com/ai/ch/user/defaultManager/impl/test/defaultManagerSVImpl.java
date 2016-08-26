157129e73b0ee0ae5072b787414cb1622816de24
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-08-26 14:55:24 +0800

    新增测试类queryDetaultInfoListTest

--- ch-user-core/src/test/java/com/ai/ch/user/defaultManager/impl/test/defaultManagerSVImpl.java
+++ ch-user-core/src/test/java/com/ai/ch/user/defaultManager/impl/test/defaultManagerSVImpl.java
@@ -11,7 +11,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ai.ch.user.api.defaultlog.interfaces.IDefaultLogSV;
+import com.ai.ch.user.api.defaultlog.params.DefaultLogVo;
 import com.ai.ch.user.api.defaultlog.params.InsertDefaultLogRequest;
+import com.ai.ch.user.api.defaultlog.params.QueryDefaultLogRequest;
+import com.ai.ch.user.api.defaultlog.params.QueryDefaultLogResponse;
+import com.ai.opt.base.vo.PageInfo;
+import com.alibaba.fastjson.JSON;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:/context/core-context.xml" })
@@ -29,6 +34,20 @@ public class defaultManagerSVImpl {
 		request.setDeductDate(new Timestamp(new Date().getTime()));
 		request.setOperId(1L);
 		defaultLogSV.insertDefaultLog(request);
+	}
+	
+	
+	@Test
+	public void queryDetaultInfoListTest(){
+		QueryDefaultLogRequest defaultLogRequest = new QueryDefaultLogRequest();
+		defaultLogRequest.setTenantId("ch");
+		defaultLogRequest.setUserId("1");
+		
+		defaultLogRequest.setPageNo(1);
+		defaultLogRequest.setPageSize(5);
+		
+		QueryDefaultLogResponse defaultLogResponse = defaultLogSV.queryDefaultLog(defaultLogRequest);
+		System.out.println(JSON.toJSONString(defaultLogResponse.getPageInfo()));
 	}
 	
 }
