35255c6955ac6448029d62f7a7019fed26b0923e
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-09-06 16:48:03 +0800

    新增接口queryAllContractInfo

--- ch-user-core/src/main/java/com/ai/ch/user/service/business/interfaces/IContractBusiSV.java
+++ ch-user-core/src/main/java/com/ai/ch/user/service/business/interfaces/IContractBusiSV.java
@@ -1,12 +1,16 @@
 package com.ai.ch.user.service.business.interfaces;
 
 
+import java.util.List;
+
 import com.ai.ch.user.api.contract.param.ContactInfoRequest;
-import com.ai.ch.user.api.contract.param.ContactInfoResponse;
+import com.ai.ch.user.api.contract.param.ContractInfoResponse;
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
 
 public interface IContractBusiSV {
 	public int insertContractInfo(ContactInfoRequest  ctContractInfo)throws BusinessException, SystemException;
-	public ContactInfoResponse queryContractInfo(ContactInfoRequest contactInfoRequest)throws BusinessException, SystemException;
+	public ContractInfoResponse queryContractInfo(ContactInfoRequest contactInfoRequest)throws BusinessException, SystemException;
+	public List<ContractInfoResponse> queryAllContractInfo(ContactInfoRequest contactInfoRequest)throws BusinessException, SystemException;
+	
 }
