590fe15b1cb9c3edc44186fb8fa34f508dc6c599
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-09-06 16:44:33 +0800

    合同返回报文体

--- ch-user-api/src/main/java/com/ai/ch/user/api/contract/param/ContractInfoResponse.java
+++ ch-user-api/src/main/java/com/ai/ch/user/api/contract/param/ContractInfoResponse.java
@@ -4,7 +4,7 @@ import java.sql.Timestamp;
 
 import com.ai.opt.base.vo.BaseResponse;
 
-public class ContactInfoResponse  extends BaseResponse{
+public class ContractInfoResponse  extends BaseResponse{
 
 	private static final long serialVersionUID = 1L;
 	/**
