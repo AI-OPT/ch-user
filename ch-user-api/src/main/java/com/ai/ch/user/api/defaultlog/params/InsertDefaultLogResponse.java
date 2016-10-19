01d3601ef7157fe079cfa46df593323920cf86dd
zhangyh7 <zhangyh7@zhangyh7.mshome.net>
2016-10-19 18:33:41 +0800

    response

---
+++ ch-user-api/src/main/java/com/ai/ch/user/api/defaultlog/params/InsertDefaultLogResponse.java
@@ -0,0 +1,20 @@
+package com.ai.ch.user.api.defaultlog.params;
+
+import com.ai.opt.base.vo.BaseResponse;
+
+public class InsertDefaultLogResponse extends BaseResponse{
+
+	private static final long serialVersionUID = 1L;
+
+	private String serialCode;
+
+	public String getSerialCode() {
+		return serialCode;
+	}
+
+	public void setSerialCode(String serialCode) {
+		this.serialCode = serialCode;
+	}
+	
+
+}
