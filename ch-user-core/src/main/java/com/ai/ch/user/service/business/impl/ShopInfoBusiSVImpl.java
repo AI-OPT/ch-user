cf9dd4c3082fd22da2dabe20ec071fa3ac4c6bec
zhangqiang7 <zhangqiang7@asiainfo.com>
2016-12-31 15:22:09 +0800

    增加非空校验

--- ch-user-core/src/main/java/com/ai/ch/user/service/business/impl/ShopInfoBusiSVImpl.java
+++ ch-user-core/src/main/java/com/ai/ch/user/service/business/impl/ShopInfoBusiSVImpl.java
@@ -93,6 +93,9 @@ public class ShopInfoBusiSVImpl implements IShopInfoBusiSV {
 
 	@Override
 	public QueryShopInfoResponse queryShopInfo(QueryShopInfoRequest request) throws BusinessException, SystemException {
+		if (request.getUserId().isEmpty()) {
+			throw new BusinessException(ExceptCodeConstants.Special.PARAM_IS_NULL, "商户id不能为空");
+		}
 		QueryShopInfoResponse response = new QueryShopInfoResponse();
 		ShopInfoCriteria example = new ShopInfoCriteria();
 		ShopInfoCriteria.Criteria criteria = example.createCriteria();
