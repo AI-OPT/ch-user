10004043f5d72cdf5589d8aecbb4f37bfecf010d
zhangqiang7 <zhangqiang7@asiainfo.com>
2017-01-09 14:27:03 +0800

    canshu

--- ch-user-core/src/main/java/com/ai/ch/user/dao/mapper/bo/CtAuditLog.java
+++ ch-user-core/src/main/java/com/ai/ch/user/dao/mapper/bo/CtAuditLog.java
@@ -3,113 +3,113 @@ package com.ai.ch.user.dao.mapper.bo;
 import java.sql.Timestamp;
 
 public class CtAuditLog {
-    private String logId;
+	private String logId;
 
-    private String tenantId;
+	private String tenantId;
 
-    private String ctType;
+	private String ctType;
 
-    private String userId;
+	private String userId;
 
-    private String auditStatus;
+	private String auditStatus;
 
-    private Timestamp auditTime;
+	private Timestamp auditTime;
 
-    private String operId;
+	private String operId;
 
-    private String operName;
+	private String operName;
 
-    private String auditDesc;
+	private String auditDesc;
 
-    private String componyName;
+	private String companyName;
 
-    private String userName;
+	private String userName;
 
-    public String getLogId() {
-        return logId;
-    }
+	public String getCompanyName() {
+		return companyName;
+	}
 
-    public void setLogId(String logId) {
-        this.logId = logId == null ? null : logId.trim();
-    }
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
 
-    public String getTenantId() {
-        return tenantId;
-    }
+	public String getLogId() {
+		return logId;
+	}
 
-    public void setTenantId(String tenantId) {
-        this.tenantId = tenantId == null ? null : tenantId.trim();
-    }
+	public void setLogId(String logId) {
+		this.logId = logId == null ? null : logId.trim();
+	}
 
-    public String getCtType() {
-        return ctType;
-    }
+	public String getTenantId() {
+		return tenantId;
+	}
 
-    public void setCtType(String ctType) {
-        this.ctType = ctType == null ? null : ctType.trim();
-    }
+	public void setTenantId(String tenantId) {
+		this.tenantId = tenantId == null ? null : tenantId.trim();
+	}
 
-    public String getUserId() {
-        return userId;
-    }
+	public String getCtType() {
+		return ctType;
+	}
 
-    public void setUserId(String userId) {
-        this.userId = userId == null ? null : userId.trim();
-    }
+	public void setCtType(String ctType) {
+		this.ctType = ctType == null ? null : ctType.trim();
+	}
 
-    public String getAuditStatus() {
-        return auditStatus;
-    }
+	public String getUserId() {
+		return userId;
+	}
 
-    public void setAuditStatus(String auditStatus) {
-        this.auditStatus = auditStatus == null ? null : auditStatus.trim();
-    }
+	public void setUserId(String userId) {
+		this.userId = userId == null ? null : userId.trim();
+	}
 
-    public Timestamp getAuditTime() {
-        return auditTime;
-    }
+	public String getAuditStatus() {
+		return auditStatus;
+	}
 
-    public void setAuditTime(Timestamp auditTime) {
-        this.auditTime = auditTime;
-    }
+	public void setAuditStatus(String auditStatus) {
+		this.auditStatus = auditStatus == null ? null : auditStatus.trim();
+	}
 
-    public String getOperId() {
-        return operId;
-    }
+	public Timestamp getAuditTime() {
+		return auditTime;
+	}
 
-    public void setOperId(String operId) {
-        this.operId = operId == null ? null : operId.trim();
-    }
+	public void setAuditTime(Timestamp auditTime) {
+		this.auditTime = auditTime;
+	}
 
-    public String getOperName() {
-        return operName;
-    }
+	public String getOperId() {
+		return operId;
+	}
 
-    public void setOperName(String operName) {
-        this.operName = operName == null ? null : operName.trim();
-    }
+	public void setOperId(String operId) {
+		this.operId = operId == null ? null : operId.trim();
+	}
 
-    public String getAuditDesc() {
-        return auditDesc;
-    }
+	public String getOperName() {
+		return operName;
+	}
 
-    public void setAuditDesc(String auditDesc) {
-        this.auditDesc = auditDesc == null ? null : auditDesc.trim();
-    }
+	public void setOperName(String operName) {
+		this.operName = operName == null ? null : operName.trim();
+	}
 
-    public String getComponyName() {
-        return componyName;
-    }
+	public String getAuditDesc() {
+		return auditDesc;
+	}
 
-    public void setComponyName(String componyName) {
-        this.componyName = componyName == null ? null : componyName.trim();
-    }
+	public void setAuditDesc(String auditDesc) {
+		this.auditDesc = auditDesc == null ? null : auditDesc.trim();
+	}
 
-    public String getUserName() {
-        return userName;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setUserName(String userName) {
-        this.userName = userName == null ? null : userName.trim();
-    }
+	public void setUserName(String userName) {
+		this.userName = userName == null ? null : userName.trim();
+	}
 }
