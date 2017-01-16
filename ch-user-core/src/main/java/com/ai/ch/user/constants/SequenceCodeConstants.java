12164519f985e49ef0a184e2491470b94d03f01f
zhangqiang7 <zhangqiang7@asiainfo.com>
2017-01-16 11:10:49 +0800

    加注解

--- ch-user-core/src/main/java/com/ai/ch/user/constants/SequenceCodeConstants.java
+++ ch-user-core/src/main/java/com/ai/ch/user/constants/SequenceCodeConstants.java
@@ -2,6 +2,8 @@ package com.ai.ch.user.constants;
 
 /**
  * SequenceCode常量
+ * 表名有中文的?
+ * 产生序列号
  */
 public class SequenceCodeConstants {
 
@@ -15,85 +17,96 @@ public class SequenceCodeConstants {
 
 		/**
 		 * uc_api_info
+		 * api表
 		 */
 		public static final String UC_API_INFO$API_SEQ_ID$SEQ = "UC_API_INFO$API_SEQ_ID$SEQ";
 
 		/**
 		 * uc_bank_info
+		 * 银行信息表
 		 */
 		public static final String UC_BANK_INFO$BANK_SEQ_ID$SEQ = "UC_BANK_INFO$BANK_SEQ_ID$SEQ";
 		/**
 		 * ct_audit_log
+		 * 审核日志表
 		 */
 		public static final String CT_AUDIT_LOG$LOG_SEQ_ID$SEQ = "CT_AUDIT_LOG$LOG_SEQ_ID$SEQ";
 
 		/**
 		 * uc_contacts_info
+		 * 合同表
 		 */
 		public static final String UC_CONTACTS_INFO$CONTACT_SEQ_ID$SEQ = "UC_CONTACTS_INFO$CONTACT_SEQ_ID$SEQ";
 
 		/**
 		 * uc_login_log
+		 * 登录信息表
 		 */
 		public static final String UC_LOGIN_LOG$LOGIN_SEQ_ID$SEQ = "UC_LOGIN_LOG$LOGIN_SEQ_ID$SEQ";
 
 		/**
 		 * uc_special_info
+		 * 特殊信息表
 		 */
 		public static final String UC_SPECIAL_INFO$INFO_SPECIAL_ID$SEQ = "UC_SPECIAL_INFO$INFO_SPECIAL_ID$SEQ";
 
 		/**
 		 * uc_state_chg
+		 * 状态表
 		 */
 		public static final String UC_STATE_CHG$STATE_CHG_ID$SEQ = "UC_STATE_CHG$STATE_CHG_ID$SEQ";
 
 		/**
 		 * uc_user
+		 * 用户表
 		 */
 		public static final String UC_USER$USER_ID$SEQ = "UC_USER$USER_ID$SEQ";
 
 		/**
 		 * uc_user_agree
+		 * 用户agree表
 		 */
 		public static final String UC_USER_AGREE$AGREE_SEQ_ID$SEQ = "UC_USER_AGREE$AGREE_SEQ_ID$SEQ";
 
 		/**
 		 * uc_user_favorite
+		 * 用户收藏信息表
 		 */
 		public static final String UC_USER_FAVORITE$FAVORITE_SEQ_ID$SEQ = "UC_USER_FAVORITE$FAVORITE_SEQ_ID$SEQ";
 
 		/**
 		 * uc_user_message
+		 * 用户信息表
 		 */
 		public static final String UC_USER_MESSAGE$INFO_SEQ_ID$SEQ = "UC_USER_MESSAGE$INFO_SEQ_ID$SEQ";
 
 		/**
 		 * uc_user_phonebooks
+		 * 通讯录信息
 		 */
 		public static final String UC_USER_PHONEBOOKS$SEQ$SEQ = "UC_USER_PHONEBOOKS$SEQ$SEQ";
 
 		/**
 		 * uc_user_safari
+		 * 浏览信息表
 		 */
 		public static final String UC_USER_SAFARI$SAFARI_SEQ_ID$SEQ = "UC_USER_SAFARI$SAFARI_SEQ_ID$SEQ";
 
 		/**
 		 * uc_user_safari_his
+		 * 浏览信息历史表
 		 */
 		public static final String UC_USER_SAFARI_HIS$SAFARI_SEQ_ID$SEQ = "UC_USER_SAFARI_HIS$SAFARI_SEQ_ID$SEQ";
 
+
 		/**
-		 * uc_user_subs
+		 * 附件表
 		 */
-		public static final String UC_USER_SUBS$CARD_SEQ_ID$SEQ = "UC_USER_SUBS$CARD_SEQ_ID$SEQ";
-
-		public static final String UC_TEL_GROUP$TEL_GROUP_ID$SEQ = "UC_TEL_GROUP$TEL_GROUP_ID$SEQ";
-
-		public static final String UC_USER_PHONEBOOKS$TEL_NO$SEQ = "UC_USER_PHONEBOOKS$TEL_NO$SEQ";
-
 		public static final String CM_CUST_FILE_EXT$INFO_EXT$ID = "CM_CUST_FILE_EXT$INFO_EXT$ID";
 		
-		
+		/**
+		 * 店铺扣款日志表
+		 */
 		public static final String SHOP_DEFAULT_LOG$SERIAL_CODE_ID$SEQ = "SHOP_DEFAULT_LOG$SERIAL_CODE_ID$SEQ";
 		
 		
